e59d387e97e3eba1a0cd5e99bd5d1a05adf4d167
ArJun <iloveyou138@vip.qq.com>
2017-10-20 21:22:58 +0800

    修改接口

--- NOW/Classes/Live/Controller/CommendViewController.m
+++ NOW/Classes/Live/Controller/CommendViewController.m
@@ -12,7 +12,7 @@
 #import "CustomButton.h"
 #import "PlayerModel.h"
 
-#define mainURL @"http://service.inke.com/api/live/aggregation?imsi=&uid=147970465&proto=6&idfa=3EDE83E7-9CD1-4186-9F37-EE77B7423265&lc=0000000000000027&cc=TG0001&imei=&sid=20tJHn0JsxdmOGkbNjpEjo3DIKFyoyboTrCjMvP7zNxofi1QNXT&cv=IK3.2.00_Iphone&devi=134a83cdf2e6701fa8f85c099c5e68ac3ea7bd4b&conn=Wifi&ua=iPhone%205s&idfv=5CCB6FE7-1F0F-4288-90DC-946D6F6C45C2&osversion=ios_9.300000&interest=1&location=0"
+#define mainURL @"http://service.inke.com/api/live/aggregation?imsi=&uid=147808343&proto=6&imei=&interest=1&location=0"
 #define Ratio 708/550
 @interface CommendViewController ()<UITableViewDelegate,UITableViewDataSource>
 @property (nonatomic, strong) UITableView *tableView;
